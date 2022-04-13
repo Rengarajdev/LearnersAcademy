e8b124ecdeaac0615710fb03d147431b4d26143b
Rengaraj <rengarajr901@gmail.com>
2022-04-13 22:25:24 +0530

    initial commit - admin module

---
+++ eclipse-workspace/Phase-2 Learners Academy-Project/src/main/java/com/LearnersAcademy/Dbconnection.java
@@ -0,0 +1,8 @@
+package com.LearnersAcademy;
+public class Dbconnection {
+	public static String userName="system";
+	public static String password="18ubc646";
+	public static String url="jdbc:oracle:thin:@localhost:1521:xe";
+	public static String driver="oracle.jdbc.driver.OracleDriver";
+
+}
